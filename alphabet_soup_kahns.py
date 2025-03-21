ed5ba87c1d848b6b408005983556576ccfb24ca4
Paul Farcasanu <pfarcasanu@gmail.com>
2025-03-21 11:14:47 -0400

    alphabet soup w/ topological sort

--- alphabet_soup_kahns.py
+++ alphabet_soup_kahns.py
@@ -1,42 +1,53 @@
 def get_alphabet(words):
+  nodes = set()
   graph = {}
+  ingraph = {}
+
+  def increment(d, c1, c2):
+    nbs = d.get(c1, set())
+    nbs.add(c2)
+    d[c1] = nbs
 
   def add_edges(word1, word2):
     for c1, c2 in zip(word1, word2):
+      nodes.add(c1)
+      nodes.add(c2)
       if c1 != c2:
-        nbs = graph.get(c1, set())
-        nbs.add(c2)
-        graph[c1] = nbs
+        increment(graph, c1, c2)
+        increment(ingraph, c2, c1)
         return
   
   for i in range(len(words)):
     for j in range(i + 1, len(words)):
       add_edges(words[i], words[j])
 
-  ## Return the longest path and char.
-  ans = {}
-  print(graph)
-  def recur(c, lcurr):
-    neighbors = graph.get(c, set())
-    if len(neighbors) == 0:
-      return lcurr
-    longest = lcurr
-    for nb in neighbors:
-      lnext = recur(nb, lcurr + 1)
-      if lnext >= longest:
-        ans[c] = nb
-        longest = lnext
-    return longest
+  indegrees = {}
+  for c in nodes:
+    nbs = ingraph.get(c, set())
+    indegrees[c] = len(nbs)
 
-  def getans(c):
-    if c in ans:
-      return c + getans(ans[c])
-    return c
+  ## Load the queue w/ indegree 0
+  q = []
+  for c in indegrees:
+    if indegrees[c] == 0:
+      q.append(c)
 
-  for c in graph.keys():
-    pathlen = recur(c, 0)
-    if pathlen == len(graph):
-      return getans(c)
+  result = []
+  while len(q) != 0:
+    curr = q.pop(0)
+    result.append(curr)
+    nbs = graph.get(curr, set())
+    for nb in nbs:
+      if nb not in indegrees:
+        continue
+      degrees = indegrees[nb] - 1
+      if degrees == 0:
+        q.append(nb)
+        del indegrees[nb]
+      else:
+        indegrees[nb] = degrees
+
+  return "".join(result)
 
 def test(num, expected):
   actual = get_alphabet(num)
